bc43ec04f981763955f19cc78cc718385267ac93
Adam Mohammed <adammoham@gmail.com>
2020-10-02 18:57:49 -0400

    Make sure to cleanup after merges

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-set -x
 
-echo "TPR: "$TRAVIS_PULL_REQUEST
-echo "EVENT:" $TRAVIS_EVENT_TYPE
+
+sudo apt-get update && sudo apt-get install -yq aws-cli libs3-2
+
+cleanup_pr () {
+    PRID=$(git log -1 | grep -oP 'Merge pull request #\K\d+')
+
+    mkdir -p ~/.aws/
+    cat <<EOF > ~/.aws/credentials
+[default]
+aws_access_key_id=$S3_ACCESS_KEY
+aws_secret_access_key=$S3_SECRET_KEY
+EOF
+    s3="aws s3 --endpoint=https://us-east-1.linodeobjects.com"
+
+    ${s3} rm s3://ci.smuggle.us/PR-${PRID}
+}
+
+
+cleanup_pr
