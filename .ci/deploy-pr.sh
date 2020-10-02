0f4f1291c063e0957dd0cbe251f6355587fab994
Adam Mohammed <adammoham@gmail.com>
2020-10-02 17:43:29 -0400

    update apt

--- .ci/deploy-pr.sh
+++ .ci/deploy-pr.sh
@@ -8,7 +8,7 @@ aws_access_key_id=$S3_ACCESS_KEY
 aws_secret_access_key=$S3_SECRET_KEY
 EOF
 
-sudo apt-get install -yq awscli libs3-2
+sudo apt-get update && sudo apt-get install -yq awscli libs3-2
 
 s3="aws s3 --endpoint=https://us-east-1.linodeobjects.com"
 
