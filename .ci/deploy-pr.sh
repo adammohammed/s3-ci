009d13492775e95c995ab4f0e2f175d056309685
Adam Mohammed <adammoham@gmail.com>
2020-10-02 17:38:07 -0400

    Try doing this?

--- .ci/deploy-pr.sh
+++ .ci/deploy-pr.sh
@@ -8,6 +8,6 @@ aws_access_key_id=$S3_ACCESS_KEY
 aws_secret_access_key=$S3_SECRET_KEY
 EOF
 
-alias s3="aws s3 --endpoint=https://us-east-1.linodeobjects.com"
+s3="aws s3 --endpoint=https://us-east-1.linodeobjects.com"
 
-s3 sync pubilc/ s3://ci.smuggle.us/$TRAVIS_BRANCH
+${s3} sync pubilc/ s3://ci.smuggle.us/$TRAVIS_BRANCH
